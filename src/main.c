261ae2a2001be323c705298f87d0692daa4451c3
Luke Kasson <luke.kasson@gmail.com>
2025-12-15 21:24:24 -0500

    command not found

--- src/main.c
+++ src/main.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+// helpful commands
+/*
+' gcc -Wall -Wextra main.c -o main.exe '
+' '
+*/
+int main()
+{
 
-int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+  // Create an integer variable that will store the number we get from the user
+  char userInput[100];
+  char errorMsg[] = ": command not found";
+  char output[200];
 
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
+  fgets(userInput, sizeof(userInput), stdin);
+
+  // Remove newline from fgets
+  userInput[strcspn(userInput, "\n")] = 0;
+
+  strcpy(output, userInput);
+  strcat(output, errorMsg);
+
+  printf("%s\n", output);
 
   return 0;
 }
