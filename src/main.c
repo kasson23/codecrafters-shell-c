62bd2702c8c9d965d276032e53d487d046f651b2
Luke Kasson <luke.kasson@gmail.com>
2025-12-17 06:45:32 -0500

    try somone elses code

--- src/main.c
+++ src/main.c
@@ -1,93 +1,36 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>  // string methods
-#include <stdbool.h> // booleans
-// helpful commands
-/*
-' gcc -Wall -Wextra main.c -o main.exe '
-' gcc -g -Wall -Wextra main.c -o main.exe ' - needs 'g' for debugging
-' .\main.exe '
-*/
-
-void remove_up_to_index(char *str, int index)
-{
-  int len = strlen(str);
-
-  // Validate index so we don't read or write out of bounds
-  if (index < 0 || index >= len)
-  {
-    return;
-  }
-
-  /*
-   * memmove(destination, source, number_of_bytes)
-   *
-   * dest = str
-   *   We want the remaining part of the string to start at index 0
-   *
-   * src = str + index
-   *   This points to the character we want to keep
-   *
-   * len - index + 1
-   *   Number of bytes to move:
-   *     - all remaining characters
-   *     - PLUS the null terminator ('\0')
-   *
-   * memmove is used instead of memcpy because
-   * source and destination overlap in memory.
-   */
-  memmove(str, str + index + 1, len - index + 1);
-}
-
+#include <string.h>
 int main()
 {
+  // Flush after every print
+  setbuf(stdout, NULL);
 
-  bool run = true;
-
-  while (run)
+  // TODO: Uncomment the code below to pass the first stage
+  bool loop_continue = true;
+  char user_input[1024];
+  printf("$ ");
+  while (loop_continue)
   {
-    char input[100];
-    char cmd[100];
+    fgets(user_input, sizeof(user_input), stdin);
 
+    user_input[strlen(user_input) - 1] = '\0';
+    if (strcmp(user_input, "exit") == 0)
+    {
+      loop_continue = false;
+      break;
+    }
+    char echoinput[5];
+    char output[20];
+    if (strcmp(memcpy(echoinput, user_input, 4), "echo") == 0)
+    {
+      puts(strcpy(output, user_input + 5));
+      printf("$ ");
+      continue;
+    }
+    printf("%s: command not found\n", user_input);
     printf("$ ");
-
-    // Wait for user input
-    fgets(input, 100, stdin);
-
-    int length = strlen(input);
-
-    // Remove the trailing newline
-    input[length - 1] = '\0';
-
-    // need to find where first ' ' is and this will be command - returns pointer
-    char *ptr = strchr(input, ' ');
-
-    if (ptr != NULL)
-    {
-      int index = (int)(ptr - input);
-
-      strncpy(cmd, input, index);
-      cmd[index] = '\0';
-
-      remove_up_to_index(input, index);
-    }
-
-    int exit = strcmp(cmd, "exit");
-    if (exit == 0)
-    {
-      run = false;
-      return 0;
-    }
-
-    int echo = strcmp(cmd, "echo");
-    if (echo == 0)
-    {
-      printf("%s\n\n", input);
-      return 0;
-    }
-
-    printf("%s: command not found\n", input);
   }
-
   return 0;
 }
